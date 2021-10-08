30043925320374bac4b7cbc6b85db9fe25a6d67d
micha vie <micha@freeiam.com>
2021-10-08 19:01:56 +0200

    first commit

---
+++ src/components/Banners/TopBrandingBanner.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+type Props = {}
+
+export const TopBrandingBanner = ({}: Props) => <div>todo</div>
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// Components
+export { TopBrandingBanner } from './components/Banners/TopBrandingBanner'
