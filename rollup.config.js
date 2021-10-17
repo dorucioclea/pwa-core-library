ef77dcb351045c55d887babf1970e25b7708f6ef
micha vie <micha@freeiam.com>
2021-10-18 01:27:31 +0200

    fix build with erdjs

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
+import json from '@rollup/plugin-json'
 import postcss from 'rollup-plugin-postcss'
 
 const packageJson = require('./package.json')
@@ -25,6 +26,7 @@ export default {
     resolve(),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
+    json(),
     postcss({
       extensions: ['.css'],
     }),
