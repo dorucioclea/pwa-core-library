91cc28a5529debae85610043dd727fde08c0a748
micha vie <micha@freeiam.com>
2021-10-24 17:32:15 +0200

    fix button default colors

--- src/features/Controls/helpers.ts
+++ src/features/Controls/helpers.ts
@@ -2,7 +2,6 @@ import { AppSystemColor } from '../../types'
 
 export const getButtonBgColorClassName = (color?: AppSystemColor, disabled?: boolean) => {
   if (disabled) return 'bg-gray-400'
-  if (!color) return ''
   if (color === 'blue') return 'bg-blue-500'
   if (color === 'red') return 'bg-red-500'
   // add more colors as needed ...
