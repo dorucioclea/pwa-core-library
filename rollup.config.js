988f019151f2ed421b80f921c68b7e4d69e4a6f6
micha vie <micha@freeiam.com>
2021-10-24 17:31:57 +0200

    fix rollup build warning

--- rollup.config.js
+++ rollup.config.js
@@ -24,7 +24,7 @@ export default {
   ],
   plugins: [
     peerDepsExternal(),
-    resolve(),
+    resolve({ preferBuiltins: true }),
     commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     json(),
