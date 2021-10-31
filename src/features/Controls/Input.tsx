0f5d18214597c98d5943d3827f3374c46f0fc2f1
micha vie <micha@freeiam.com>
2021-10-31 21:30:05 +0100

    fix input props type intersection

--- src/features/Controls/Input.tsx
+++ src/features/Controls/Input.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, InputHTMLAttributes } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition } from '@fortawesome/fontawesome-common-types'
+import { IntersectProps } from '../../types'
 
 type Props = {
   onChange: (value: string) => void
@@ -11,7 +12,7 @@ type Props = {
 const DefaultClassNames =
   'focus:outline-none border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 bg-gray-200 w-full appearance-none leading-normal block px-4 py-2 max-w-sm rounded transition duration-300'
 
-export const Input = (props: Props & InputHTMLAttributes<HTMLInputElement>) =>
+export const Input = (props: IntersectProps<InputHTMLAttributes<HTMLInputElement>, Props>) =>
   props.icon ? (
     <div className={props.className}>
       <div className="relative">
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,3 @@
-export type AppSystemColor =
-  | 'primary'
-  | 'black'
-  | 'white'
-  | 'gray'
-  | 'red'
-  | 'yellow'
-  | 'green'
-  | 'teal'
-  | 'blue'
-  | 'indigo'
-  | 'purple'
-  | 'pink'
+export type IntersectProps<T1, T2> = Omit<T1, keyof T2> & T2
+
+export type AppSystemColor = 'primary' | 'black' | 'white' | 'gray' | 'red' | 'yellow' | 'green' | 'teal' | 'blue' | 'indigo' | 'purple' | 'pink'
