7d4334a594ba7d1b72919e1c18b1ce14838bf1fd
micha vie <micha@freeiam.com>
2021-10-31 21:40:01 +0100

    update input styles

--- src/features/Controls/Input.tsx
+++ src/features/Controls/Input.tsx
@@ -10,7 +10,7 @@ type Props = {
 }
 
 const DefaultClassNames =
-  'focus:outline-none border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 bg-gray-200 w-full appearance-none leading-normal block px-4 py-2 max-w-sm rounded transition duration-300'
+  'focus:outline-none border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 bg-gray-200 w-full appearance-none leading-normal block px-4 py-2 text-xl rounded-xl transition duration-300'
 
 export const Input = (props: IntersectProps<InputHTMLAttributes<HTMLInputElement>, Props>) =>
   props.icon ? (
