5e1bf3aaeddd125942fe488fb909b9d63d8f2a3a
micha vie <micha@freeiam.com>
2021-11-11 17:45:41 +0100

    add scy nft request

--- src/features/Superciety/api.ts
+++ src/features/Superciety/api.ts
@@ -1,7 +1,10 @@
 import { IHttpService } from '../../services/http'
 import { NftCollectionList } from '../Tokens/types'
 import { ScyIdentity } from './types'
+import { Nft, TokenIdentifier } from '../Tokens/types'
 
 export const getScyIdentityRequest = async (http: IHttpService, id: string) => await http.get<ScyIdentity>(`identity/${id}`)
 
 export const getScyNftsByUserRequest = async (http: IHttpService, userId: string) => await http.get<NftCollectionList>(`nfts?owner=${userId}`)
+
+export const getScyNftRequest = async (httpService: IHttpService, id: TokenIdentifier) => await httpService.get<Nft>(`nfts/${id}`)
