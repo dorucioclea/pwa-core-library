302c2e008887563e9cbde4f38153f1c42b53ae8f
micha vie <micha@freeiam.com>
2021-11-13 15:28:45 +0100

    update branding banner

--- src/features/Banners/BrandingBanner.tsx
+++ src/features/Banners/BrandingBanner.tsx
@@ -1,21 +1,40 @@
 import React from 'react'
+import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const SupercietyWebUrl = 'https://superciety.com'
 
 export const BrandingBanner = () => (
-  <div className="bg-black p-2">
-    <div className="max-w-4xl mx-auto flex justify-around text-white text-lg">
-      <a href="#" className="font-head font-bold">
-        Superciety
-      </a>
-      <a href="#" target="_blank">
-        <span className="mr-2">Join the Superciety DAO on Elrond</span>
-        <svg xmlns="http://www.w3.org/2000/svg" className="inline h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </a>
-    </div>
+  <div className="bg-black flex justify-center p-2 text-xl text-white">
+    <a href={SupercietyWebUrl} target="_blank" className="space-x-2" style={{ borderWidth: 0 }}>
+      <span>A service by</span>
+      <strong>Superciety</strong>
+      <span className="inline-block -mt-1">
+        <SupercietyLogoWhite />
+      </span>
+      <span>Join us on Elrond</span>
+      <FontAwesomeIcon icon={faAngleDoubleRight} className="text-white" />
+    </a>
   </div>
+)
+
+const SupercietyLogoWhite = () => (
+  <svg viewBox="0 0 402 402" fill="none" xmlns="http://www.w3.org/2000/svg" className="inline-block w-6">
+    <circle cx={260.215} cy={82.023} r={16} transform="rotate(-45 260.215 82.023)" fill="#fff" />
+    <circle cx={379.009} cy={200.817} r={16} transform="rotate(-45 379.009 200.817)" fill="#fff" />
+    <circle cx={141.421} cy={200.817} r={16} transform="rotate(-45 141.421 200.817)" fill="#fff" />
+    <circle cx={260.215} cy={319.611} r={16} transform="rotate(-45 260.215 319.611)" fill="#fff" />
+    <circle cx={200.818} cy={141.421} r={16} transform="rotate(-45 200.818 141.421)" fill="#fff" />
+    <circle cx={319.612} cy={260.214} r={16} transform="rotate(-45 319.612 260.214)" fill="#fff" />
+    <circle cx={82.024} cy={260.214} r={16} transform="rotate(-45 82.024 260.214)" fill="#fff" />
+    <circle cx={200.818} cy={379.009} r={16} transform="rotate(-45 200.818 379.009)" fill="#fff" />
+    <circle cx={141.421} cy={82.023} r={16} transform="rotate(-45 141.421 82.023)" fill="#fff" />
+    <circle cx={260.215} cy={200.817} r={16} transform="rotate(-45 260.215 200.817)" fill="#fff" />
+    <circle cx={22.627} cy={200.817} r={16} transform="rotate(-45 22.627 200.817)" fill="#fff" />
+    <circle cx={141.421} cy={319.611} r={16} transform="rotate(-45 141.421 319.611)" fill="#fff" />
+    <circle cx={200.818} cy={22.627} r={16} transform="rotate(-45 200.818 22.627)" fill="#fff" />
+    <circle cx={319.612} cy={141.421} r={16} transform="rotate(-45 319.612 141.421)" fill="#fff" />
+    <circle cx={82.024} cy={141.421} r={16} transform="rotate(-45 82.024 141.421)" fill="#fff" />
+    <circle cx={200.818} cy={260.214} r={16} transform="rotate(-45 200.818 260.214)" fill="#fff" />
+  </svg>
 )
