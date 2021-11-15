ee36294bd630986477ab087f32c66ca31850b9ab
micha vie <micha@freeiam.com>
2021-11-16 00:12:14 +0100

    add button indigo color

--- src/features/Controls/helpers.ts
+++ src/features/Controls/helpers.ts
@@ -6,6 +6,7 @@ export const getButtonBgColorClassName = (color?: AppSystemColor, disabled?: boo
   if (color === 'red') return 'bg-red-500'
   if (color === 'gray') return 'bg-gray-800'
   if (color === 'black') return 'bg-black'
+  if (color === 'indigo') return 'bg-indigo-500'
   // add more colors as needed ...
   return 'bg-primary-400'
 }
