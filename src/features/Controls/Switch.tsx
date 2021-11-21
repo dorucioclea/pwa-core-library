7e86bbc85ddd0542ff3fbf0a4763710262396f66
micha vie <micha@freeiam.com>
2021-11-21 19:08:57 +0100

    refactor switch control

--- src/features/Controls/Switch.tsx
+++ src/features/Controls/Switch.tsx
@@ -1,28 +1,32 @@
 import React from 'react'
-import BaseSwitch from 'react-switch'
+import { classNames } from '../../helpers'
 
 type IProps = {
+  label: string
   checked: boolean
   onChange: (isChecked: boolean) => void
-  color?: string
-  colorHandle?: string
   className?: string
 }
 
 export const Switch = (props: IProps) => (
-  <div className={props.className}>
-    <BaseSwitch
-      checked={props.checked}
-      onChange={(checked) => props.onChange(checked)}
-      onColor={props.color || '#9AE6B4'}
-      onHandleColor={props.colorHandle || '#48BB78'}
-      handleDiameter={25}
-      uncheckedIcon={false}
-      checkedIcon={false}
-      boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-      activeBoxShadow="0px 0px 1px 5px rgba(0, 0, 0, 0.2)"
-      height={20}
-      width={48}
-    />
-  </div>
+  <button
+    type="button"
+    onClick={() => props.onChange(!props.checked)}
+    className={classNames(
+      'relative inline-flex flex-shrink-0 h-8 w-14 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-green-100',
+      props.checked ? 'bg-green-400' : 'bg-gray-200',
+      props.className || ''
+    )}
+    role="switch"
+    aria-checked="false"
+  >
+    <span className="sr-only">{props.label}</span>
+    <span
+      aria-hidden="true"
+      className={classNames(
+        'pointer-events-none inline-block h-7 w-7 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
+        props.checked ? 'translate-x-6' : 'translate-x-0'
+      )}
+    ></span>
+  </button>
 )
