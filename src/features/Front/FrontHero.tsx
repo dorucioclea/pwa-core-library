2258aa191f5c1ac376a1e3a19abc02e7b380f51c
micha vie <micha@freeiam.com>
2021-12-05 17:09:31 +0100

    fix front hero on mobile

--- src/features/Front/FrontHero.tsx
+++ src/features/Front/FrontHero.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { LinkButton } from '../Controls/LinkButton'
+import { classNames } from '../../helpers'
 
 type Props = {
   title: string
@@ -25,11 +26,11 @@ export const FrontHero = (props: Props) => {
 
   return (
     <section
-      className={`relative flex flex-col ${!!props.children ? 'justify-around' : 'pt-8 md:pt-20 lg:pt-32'} ${props.className || ''}`}
+      className={classNames('relative flex flex-col', !!props.children ? 'justify-around' : 'pt-8 md:pt-20 lg:pt-32', props.className)}
       style={{ minHeight: props.reduceHeightBy ? `calc(100vh - 100px - ${props.reduceHeightBy}px)` : 'calc(100vh - 100px)' }}
     >
       <div className="max-w-3xl">
-        <h1 className={`text-8xl mb-4 ${titleColorClassNames}`}>{props.title}</h1>
+        <h1 className={classNames('text-6xl sm:text-7xl md:text-8xl mb-4', titleColorClassNames)}>{props.title}</h1>
         <p className="text-gray-600 text-3xl mb-8">{props.description}</p>
         {props.ctaText && props.ctaLink && (
           <LinkButton href={props.ctaLink}>
@@ -38,11 +39,16 @@ export const FrontHero = (props: Props) => {
           </LinkButton>
         )}
       </div>
-      <div className={`absolute right-0 top-4 -z-10 -mr-32 opacity-10 sm:opacity-50 lg:opacity-75 xl:opacity-100 ${props.imageContainerClassName}`}>
+      <div
+        className={classNames(
+          'absolute right-0 top-4 -z-10 -mr-32 opacity-10 sm:opacity-50 lg:opacity-75 xl:opacity-100',
+          props.imageContainerClassName
+        )}
+      >
         <img src={props.image} className={props.imageClassName} />
       </div>
       {props.children && <div className="flex justify-center items-center text-3xl text-gray-600">{props.children}</div>}
-      <footer className="absolute bottom-0 left-0 right-0 h-12 flex justify-center items-center mt-40 sm:mt-32 md:mt-16 lg:mt-0">
+      <footer className="relative md:absolute bottom-0 left-0 right-0 h-32 sm:h-20 md:h-12 flex justify-center items-center">
         {props.footerContent}
       </footer>
     </section>
