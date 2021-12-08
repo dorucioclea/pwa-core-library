d1b329e2d21fa6f8014422cf4fbc551f99ed90f2
micha vie <micha@freeiam.com>
2021-12-08 06:22:45 +0100

    update tx processing indicator overlay

--- src/features/Transactions/TxProcessingIndicatorOverlay.tsx
+++ src/features/Transactions/TxProcessingIndicatorOverlay.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { classNames } from '../..'
 import { TxProcessingIndicator } from './TxProcessingIndicator'
 
-export const TxProcessingIndicatorOverlay = () => (
-  <div className="absolute z-50 inset-0 bg-white bg-opacity-90 flex flex-col justify-center items-center -mt-2">
+type Props = {
+  className?: string
+}
+
+export const TxProcessingIndicatorOverlay = (props: Props) => (
+  <div className={classNames('absolute z-50 inset-0 bg-white bg-opacity-90 flex flex-col justify-center items-center -mt-2', props.className)}>
     <TxProcessingIndicator className="mb-8" />
     <p className="text-gray-800 text-2xl animate-pulse">Processing your transaction on the blockchain ...</p>
   </div>
