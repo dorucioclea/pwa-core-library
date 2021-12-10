e33ec439809385f44a1642bec9a994d9a9332bf4
micha vie <micha@freeiam.com>
2021-12-10 11:36:55 +0100

    update super test tokens

--- src/features/Tokens/helper.ts
+++ src/features/Tokens/helper.ts
@@ -9,7 +9,7 @@ export const getTokenTypeDisplayName = (tokenType: string) => {
 
 export const getSuperToken = (networkId: string): Token => {
   const network = getBlockchainNetworkFromId(networkId)
-  if (network === 'devnet') return new Token({ identifier: 'XSUPER-d0da40', name: 'xSuper', decimals: 0 })
-  if (network === 'testnet') return new Token({ identifier: 'XSUPER-72a15d', name: 'xSuper', decimals: 0 })
+  if (network === 'devnet') return new Token({ identifier: 'XSUPER-d0da40', name: 'XSuper', decimals: 0 })
+  if (network === 'testnet') return new Token({ identifier: 'XSUPER-34d9ea', name: 'XSuper', decimals: 0 })
   return new Token({ identifier: 'SUPER-764d8d', name: 'Super', decimals: 0 })
 }
