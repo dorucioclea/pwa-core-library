78467c6a94462228b9f5e897df149c1e22cb22b8
micha vie <micha@freeiam.com>
2021-12-16 18:03:06 +0100

    fix nav coming-soon non-clickable

--- src/features/Layout/_NavigationItem.tsx
+++ src/features/Layout/_NavigationItem.tsx
@@ -6,13 +6,14 @@ export const _NavigationItem = (props: NavigationItem) => {
   const className = 'items-center flex-col pt-4 md:justify-center md:pt-0 md:flex text-gray-500 text-xl hover:text-primary-500 hover:no-underline'
   const style = { transition: 'all .2s ease-in' }
   const tooltip = props.soon ? 'coming soon' : undefined
+  const href = props.soon ? '#' : props.href
 
   return props.href.startsWith('http') ? (
-    <a data-tip={tooltip} href={props.href} target="_blank" rel="noopener" className={className} style={style}>
+    <a data-tip={tooltip} href={href} target="_blank" rel="noopener" className={className} style={style}>
       {props.text}
     </a>
   ) : (
-    <Link href={props.href} as={props.as}>
+    <Link href={href} as={props.as}>
       <a data-tip={tooltip} className={className} style={style}>
         {props.text}
       </a>
