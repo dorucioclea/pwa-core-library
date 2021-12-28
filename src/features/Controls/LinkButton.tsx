741ab7ae15cb6a41bc7a73e97f8ded42789eab35
micha vie <micha@freeiam.com>
2021-12-28 16:00:31 +0100

    add large prop to link button

--- src/features/Controls/LinkButton.tsx
+++ src/features/Controls/LinkButton.tsx
@@ -13,14 +13,23 @@ type Props = {
   disabled?: boolean
   submit?: boolean
   loading?: boolean
+  large?: boolean
   className?: string
   style?: CSSProperties
 }
 
 export const LinkButton = (props: Props) => {
-  const baseClassNames = 'relative inline-flex justify-center items-center py-2 px-6 rounded-xl shadow text-xl text-white transition duration-300'
+  const baseClassNames = 'relative inline-flex justify-center items-center shadow text-white transition duration-300'
   const stateClassNames = 'hover:shadow-lg focus:outline-none focus:shadow-outline focus:shadow-none'
-  const className = classNames(baseClassNames, getButtonBgColorClassName(props.color, props.disabled), stateClassNames, props.className)
+  const sizingClassNames = props.large ? 'text-2xl px-8 py-4 rounded-3xl' : 'text-xl px-6 py-2 rounded-xl'
+
+  const className = classNames(
+    baseClassNames,
+    getButtonBgColorClassName(props.color, props.disabled),
+    stateClassNames,
+    sizingClassNames,
+    props.className
+  )
 
   const Content = () => (
     <>
