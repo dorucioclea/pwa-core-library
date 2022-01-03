e771f1169e6a702da4c78643942b6e39dae34236
micha vie <micha@freeiam.com>
2022-01-03 01:08:08 +0100

    decrease mobile nav bottom padding

--- src/features/Layout/NavigationMobile.tsx
+++ src/features/Layout/NavigationMobile.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavigationItem } from './types'
+import { classNames } from '../../helpers'
 import { _NavigationMobileItem } from './_NavigationMobileItem'
 
 type Navigation = {
@@ -8,7 +9,7 @@ type Navigation = {
 }
 
 export const NavigationMobile = (props: Navigation) => (
-  <nav className={`fixed bottom-0 left-0 right-0 flex-grow z-50 pb-6 bg-white rounded-t-lg md:hidden ${props.className || ''}`}>
+  <nav className={classNames('fixed bottom-0 left-0 right-0 flex-grow z-50 pb-2 bg-white rounded-t-lg md:hidden', props.className)}>
     <ul className="flex h-16">
       {props.items.map((item, index) => (
         <li key={index} className="relative flex items-center justify-center flex-grow">
