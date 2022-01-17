d0d58faaa8940e2524935391e06f18d141913560
micha vie <micha@freeiam.com>
2022-01-17 17:18:31 +0100

    increase default size of ellipsis loader

--- src/features/Loaders/EllipsisLoader.tsx
+++ src/features/Loaders/EllipsisLoader.tsx
@@ -5,45 +5,13 @@ interface IProps {
 }
 
 export const EllipsisLoader = ({ className }: IProps) => (
-  <svg
-    viewBox="0 0 120 30"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    className={className || 'text-primary-500 w-10'}
-  >
+  <svg viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className={className || 'text-primary-500 w-12'}>
     <circle cx="15" cy="15" r="12.795">
-      <animate
-        attributeName="r"
-        from="15"
-        to="15"
-        begin="0s"
-        dur="0.8s"
-        values="15;9;15"
-        calcMode="linear"
-        repeatCount="indefinite"
-      ></animate>{' '}
-      <animate
-        attributeName="fillOpacity"
-        from="1"
-        to="1"
-        begin="0s"
-        dur="0.8s"
-        values="1;.5;1"
-        calcMode="linear"
-        repeatCount="indefinite"
-      ></animate>
+      <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite"></animate>{' '}
+      <animate attributeName="fillOpacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite"></animate>
     </circle>{' '}
     <circle cx="60" cy="15" r="11.205" fillOpacity="0.3">
-      <animate
-        attributeName="r"
-        from="9"
-        to="9"
-        begin="0s"
-        dur="0.8s"
-        values="9;15;9"
-        calcMode="linear"
-        repeatCount="indefinite"
-      ></animate>{' '}
+      <animate attributeName="r" from="9" to="9" begin="0s" dur="0.8s" values="9;15;9" calcMode="linear" repeatCount="indefinite"></animate>{' '}
       <animate
         attributeName="fillOpacity"
         from="0.5"
@@ -56,26 +24,8 @@ export const EllipsisLoader = ({ className }: IProps) => (
       ></animate>
     </circle>{' '}
     <circle cx="105" cy="15" r="12.795">
-      <animate
-        attributeName="r"
-        from="15"
-        to="15"
-        begin="0s"
-        dur="0.8s"
-        values="15;9;15"
-        calcMode="linear"
-        repeatCount="indefinite"
-      ></animate>{' '}
-      <animate
-        attributeName="fillOpacity"
-        from="1"
-        to="1"
-        begin="0s"
-        dur="0.8s"
-        values="1;.5;1"
-        calcMode="linear"
-        repeatCount="indefinite"
-      ></animate>
+      <animate attributeName="r" from="15" to="15" begin="0s" dur="0.8s" values="15;9;15" calcMode="linear" repeatCount="indefinite"></animate>{' '}
+      <animate attributeName="fillOpacity" from="1" to="1" begin="0s" dur="0.8s" values="1;.5;1" calcMode="linear" repeatCount="indefinite"></animate>
     </circle>
   </svg>
 )
