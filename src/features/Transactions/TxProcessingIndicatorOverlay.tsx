ccfc4968c80e27c2caa7da5ebfa61a14f76afd64
micha vie <micha@freeiam.com>
2022-01-21 15:01:28 +0100

    add optional text to tx processing indicator overlay

--- src/features/Transactions/TxProcessingIndicatorOverlay.tsx
+++ src/features/Transactions/TxProcessingIndicatorOverlay.tsx
@@ -4,11 +4,12 @@ import { TxProcessingIndicator } from './TxProcessingIndicator'
 
 type Props = {
   className?: string
+  text?: string
 }
 
 export const TxProcessingIndicatorOverlay = (props: Props) => (
   <div className={classNames('absolute z-50 inset-0 bg-white bg-opacity-90 flex flex-col justify-center items-center -mt-2', props.className)}>
     <TxProcessingIndicator className="mb-8" />
-    <p className="text-gray-800 text-2xl animate-pulse">Processing your transaction on the blockchain ...</p>
+    <p className="text-gray-800 text-2xl animate-pulse">{props.text || 'Processing your transaction on the blockchain ...'}</p>
   </div>
 )
