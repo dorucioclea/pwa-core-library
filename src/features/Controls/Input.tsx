26c1e9b333ca8ea017c06b50436e8bf9dd6612fa
micha vie <micha@freeiam.com>
2022-04-08 15:24:33 +0200

    add input component ref forwarding

--- src/features/Controls/Input.tsx
+++ src/features/Controls/Input.tsx
@@ -1,8 +1,8 @@
-import React, { ChangeEvent, InputHTMLAttributes } from 'react'
+import React, { ChangeEvent, forwardRef } from 'react'
+import { classNames } from '../../helpers'
+import { IntersectProps } from '../../types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition } from '@fortawesome/fontawesome-common-types'
-import { IntersectProps } from '../../types'
-import { classNames } from '../../helpers'
 
 type Props = {
   onChange: (value: string) => void
@@ -11,10 +11,12 @@ type Props = {
   className?: string
 }
 
+type InputHtmlElement = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+
 const DefaultClassNames =
   'focus:outline-none border border-transparent focus:bg-white focus:border-gray-300 placeholder-gray-600 bg-gray-200 w-full appearance-none leading-normal block px-4 py-2 text-xl rounded-xl transition duration-300'
 
-export const Input = (props: IntersectProps<InputHTMLAttributes<HTMLInputElement>, Props>) => {
+export const Input = forwardRef((props: IntersectProps<InputHtmlElement, Props>, ref: React.ForwardedRef<HTMLInputElement>) => {
   const showCharLimit = !!props.value && !!props.maxLength && props.type !== 'number' && props.maxLength > 1
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
@@ -27,12 +29,17 @@ export const Input = (props: IntersectProps<InputHTMLAttributes<HTMLInputElement
           {props.icon && <FontAwesomeIcon icon={props.icon} className="text-gray-600" />}
           {props.pre && <span className="font-head text-gray-600">{props.pre}</span>}
         </div>
-        <input {...props} onChange={handleChange} className={classNames(DefaultClassNames, props.className, props.pre ? 'pl-16' : 'pl-10')} />
+        <input
+          {...props}
+          ref={ref}
+          onChange={handleChange}
+          className={classNames(DefaultClassNames, props.className, props.pre ? 'pl-16' : 'pl-10')}
+        />
       </div>
     </div>
   ) : (
     <div className={classNames('relative w-full', props.className)}>
-      <input {...props} onChange={handleChange} className={DefaultClassNames} />
+      <input {...props} ref={ref} onChange={handleChange} className={DefaultClassNames} />
       {showCharLimit && (
         <small className="absolute right-2 -bottom-6 text-gray-500 text-sm">
           ({(props.value as string).length} / {props.maxLength})
@@ -40,4 +47,4 @@ export const Input = (props: IntersectProps<InputHTMLAttributes<HTMLInputElement
       )}
     </div>
   )
-}
+})
