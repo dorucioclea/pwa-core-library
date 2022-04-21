7db339b0f3de4da05e04b010f388c7923e1869a6
micha vie <micha@freeiam.com>
2022-04-21 11:13:10 +0200

    fix dev mode banner for ssr

--- src/features/Banners/DevModeBanner.tsx
+++ src/features/Banners/DevModeBanner.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const FaucetLinkUrl = 'https://r3d4.fr/faucet'
 
 export const DevModeBanner = () => {
-  if (typeof window === 'undefined') return null
-  const hostname = window.location.hostname
+  const [hostname, setHostname] = useState<string | null>(null)
+
+  useEffect(() => {
+    setHostname(window.location.hostname)
+  }, [])
+
+  if (!hostname) {
+    return null
+  }
+
   const fragments = hostname.match(/^(?:.*?\.)?([a-zA-Z0-9\-_]{3,}\.(?:\w{2,8}|\w{2,4}\.\w{2,4}))$/)
   const domain = !!fragments ? fragments[1] : null
-
-  const getChainName = () => {
-    if (hostname.includes('devnet')) return 'devnet'
-    if (hostname.includes('testnet')) return 'testnet'
-    return 'test'
-  }
 
   return (
     <div className="bg-gradient-to-r from-red-500 to-pink-500 p-2 text-center">
       <p className="text-white text-lg">
-        This is the {getChainName()} version of{' '}
+        This is the {getChainName(hostname)} version of{' '}
         <a href={`https://${domain}`} className="text-white hover:text-red-100 font-bold" style={{ borderBottomWidth: 1, borderColor: '#fff' }}>
           {domain}
         </a>
@@ -34,4 +36,10 @@ export const DevModeBanner = () => {
       </p>
     </div>
   )
+}
+
+const getChainName = (host: string) => {
+  if (host.includes('devnet')) return 'devnet'
+  if (host.includes('testnet')) return 'testnet'
+  return 'test'
 }
