cabff4408583021bc5a200c994077ec15ddb8d0f
micha vie <micha@freeiam.com>
2022-05-06 12:22:10 +0200

    fix nav mobile z-index

--- src/features/Layout/NavigationMobile.tsx
+++ src/features/Layout/NavigationMobile.tsx
@@ -11,9 +11,10 @@ type Navigation = {
 export const NavigationMobile = (props: Navigation) => (
   <nav
     className={classNames(
-      'fixed bottom-0 left-0 right-0 flex-grow z-50 bg-white rounded-t-lg md:hidden shadow-inner border-t border-gray-100',
+      'fixed bottom-0 left-0 right-0 flex-grow bg-white rounded-t-lg md:hidden shadow-inner border-t border-gray-100',
       props.className
     )}
+    style={{ zIndex: 999 }}
   >
     <ul className="flex h-20">
       {props.items.map((item, index) => (
