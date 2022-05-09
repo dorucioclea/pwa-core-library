9275774019525f72b72ad373d7059700ee653212
micha vie <micha@freeiam.com>
2022-05-09 19:39:16 +0200

    update markdown styles

--- src/features/Controls/Markdown.tsx
+++ src/features/Controls/Markdown.tsx
@@ -10,7 +10,11 @@ export const Markdown = (props: Props) => (
   <MarkdownComponent
     linkTarget="_blank"
     components={{
-      h1: 'h2',
+      h1: ({ node, ...props }) => <h2 className="mt-4" {...props} />,
+      h2: ({ node, ...props }) => <h3 className="mt-4" {...props} />,
+      h3: ({ node, ...props }) => <h4 className="mt-4" {...props} />,
+      h4: ({ node, ...props }) => <h5 className="mt-4" {...props} />,
+      h5: ({ node, ...props }) => <h6 className="mt-4" {...props} />,
       p: ({ node, ...props }) => <p className="py-2 text-xl md:text-2xl" {...props} />,
       blockquote: ({ node, ...props }) => (
         <blockquote className="my-4 border-l-4 border-primary-400 bg-gray-50 px-4 py-2 rounded-lg text-lg" {...props} />
